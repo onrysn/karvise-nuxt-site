c4861ced1b0c5c312b730b93e1b625fc96ad17ba
onur-yasin <onur.yasin@utilitek.com.tr>
2025-09-01 10:53:13 +0300

    bug fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,29 @@ export default defineNuxtConfig({
   modules: [
     '@primevue/nuxt-module'
   ],
+  app: {
+    head: {
+      titleTemplate: "%s - Karvise",
+      title: "Karvise",
+      meta: [
+        {
+          name: "description",
+          content: "Karvise yazılım çözümleri ile işinizi büyütün. Özel yazılım ve dijital hizmetler."
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1"
+        },
+        {
+          charset: "utf-8"
+        }
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
+      ],
+      htmlAttrs: {
+        lang: 'tr' // Sayfanın dili
+      },
+    },
+  }
 });
