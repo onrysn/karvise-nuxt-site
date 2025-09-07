50cc5941cbcf758dfeeaaad3de3fe431ca2adb91
Onur Yasin <onuryasin1999@gmail.com>
2025-09-08 01:24:17 +0300

    ..

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  nitro: {
+    experimental: {
+      openAPI: true
+    }
+  },
 
-
-  // Uyumluluk
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
